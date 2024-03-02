14b029874eeb51358350b02d7c4410a24934ccd1
yusufkara <s215824@dtu.dk>
2024-03-02 12:31:28 +0100

    HTML and CSS

--- src/components/user_information/UserInformation.tsx
+++ src/components/user_information/UserInformation.tsx
@@ -26,7 +26,7 @@ export const UserInformation = () => {
       setEmailValid(validateEmail(value));
     }
 
-    if (name === 'phone') {
+    if (name === 'phone' || name === 'vatNumber') {
       if (!/^\d*$/.test(value) || value.length > 8) {
         return; 
       }
@@ -37,43 +37,56 @@ export const UserInformation = () => {
 
   return (
     <div>
+      <label className="input-label">Full Name <span className="required-asterisk">*</span></label>      
       <input
         type="text"
         name="name"
         value={userInfo.name}
         onChange={handleInputChange}
-        placeholder="Full Name"
+        placeholder="Full name"
+        className="input-field"
+        required
       />
+      <label className="input-label">Phone <span className="required-asterisk">*</span></label>      
       <input
         type="tel"
         name="phone"
         value={userInfo.phone}
         onChange={handleInputChange}
-        placeholder="Phone e.g., 40964767"
+        placeholder="Phone e.g., 40464269"
         pattern="[0-9]*"
         inputMode="numeric"
+        className="input-field"
+        required
       />
+      <label className="input-label">Email Address <span className="required-asterisk">*</span></label>      
       <input
         type="email"
         name="email"
         value={userInfo.email}
         onChange={handleInputChange}
-        placeholder="Email Address"
+        placeholder="E-mail e.g., vita@vita.com"
+        className="input-field"
+        required
       />
       {!emailValid && <p className="email-error">Please enter a valid email address.</p>}
+      <label className="input-label">Company Name</label>      
       <input
         type="text"
         name="companyName"
         value={userInfo.companyName}
         onChange={handleInputChange}
         placeholder="Company Name"
+        className="input-field"
       />
+      <label className="input-label">Vat Number</label>      
       <input
         type="text"
         name="vatNumber"
         value={userInfo.vatNumber}
         onChange={handleInputChange}
         placeholder="VAT Number (8 digits for Denmark)"
+        className="input-field"
       />
     </div>
   );
