5b898ee3004a89be8a081c17bb95e9c6d1e2ca07
Yusuf Kara <yusufkarasan@hotmail.com>
2024-03-03 11:45:54 +0100

    Total Amount

--- src/components/total_amount/TotalAmount.tsx
+++ src/components/total_amount/TotalAmount.tsx
@@ -7,41 +7,52 @@ interface TotalAmountProps {
 }
 
 const TotalAmount: React.FC<TotalAmountProps> = ({ cart }) => {
-  // State for total amount
   const [totalAmount, setTotalAmount] = useState<number>(0);
+  const [originalTotal, setOriginalTotal] = useState<number>(0);
 
-  // Calculate total amount
   useEffect(() => {
-    // Calculate total amount without discount
-    const calculatedTotal = cart.reduce((total, cartItem) => {
-      return total + cartItem.price * cartItem.quantity;
-    }, 0);
+    let newTotal = 0;
+    let newOriginalTotal = 0;
 
-    // Apply discount if total amount is over 300 DKK
-    const discountThreshold = 300;
-    const discountedTotal = calculatedTotal >= discountThreshold ? calculatedTotal * 0.9 : calculatedTotal;
+    cart.forEach(cartItem => {
+      const itemTotalWithoutRebate = cartItem.price * cartItem.quantity;
+      const rebate = cartItem.quantity >= cartItem.rebateQuantity 
+                     ? itemTotalWithoutRebate * (cartItem.rebatePercent / 100) 
+                     : 0;
+      newTotal += itemTotalWithoutRebate - rebate;
+      newOriginalTotal += itemTotalWithoutRebate;
+    });
 
-    // Update total amount state
-    setTotalAmount(discountedTotal);
-    
+    setTotalAmount(newTotal);
+    setOriginalTotal(newOriginalTotal);
   }, [cart]);
 
-  // Separate the currency code and amount for individual styling
   const currencyCode = "DKK";
   const formattedTotalAmount = new Intl.NumberFormat("en-AU", {
     style: "currency",
     currency: "DKK",
     minimumFractionDigits: 2,
-  })
-    .format(totalAmount)
-    .replace("DKK", "")
-    .trim();
+  }).format(totalAmount).replace("DKK", "").trim();
+
+  const formattedOriginalTotalAmount = new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency: "DKK",
+    minimumFractionDigits: 2,
+  }).format(originalTotal).replace("DKK", "").trim();
 
   return (
     <div className="total-amount-container">
       <h2 className="total">Total</h2>
-      <span className="currency-code">{currencyCode}</span>
-      <span className="amount">{formattedTotalAmount}</span>
+      {totalAmount !== originalTotal && (
+        <div className="original-amount">
+          <span className="currency-code">{currencyCode}</span>
+          <span className="amount strikethrough">{formattedOriginalTotalAmount}</span>
+        </div>
+      )}
+      <div>
+        <span className="currency-code">{currencyCode}</span>
+        <span className="amount">{formattedTotalAmount}</span>
+      </div>
     </div>
   );
 };
