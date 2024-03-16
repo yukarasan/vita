feb2ac418d0a7e40887c5e3d13e4f448db40f2b7
yusufkara <s215824@dtu.dk>
2024-03-16 10:07:10 +0100

    Refactoring of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './assets/styles/App.css';
 import CartList from './components/cart_list/CartList';
 import TotalAmount from './components/total_amount/TotalAmount';
@@ -8,32 +8,12 @@ import { DeliveryAddress } from './components/delivery_address/DeliveryAddress';
 
 function App() {
   const [cart, setCart] = useState<CartItemType[]>([]);
-  const [userInfo, setUserInfo] = useState({
-    name: '',
-    phone: '',
-    email: '',
-    companyName: '',
-    vatNumber: '',
-  });
-  const [termsAccepted, setTermsAccepted] = useState(false);
 
   useEffect(() => {
     const initialCart = Cart.getInitialCart().map(item => {
-      let recurringOrder: 'none' | 'weekly' | 'monthly';
-      switch (item.recurringOrder) {
-        case 'weekly':
-          recurringOrder = 'weekly';
-          break;
-        case 'monthly':
-          recurringOrder = 'monthly';
-          break;
-        default:
-          recurringOrder = 'none';
-      }
 
       return {
-        ...item,
-        recurringOrder
+        ...item
       };
     });
 
@@ -42,39 +22,6 @@ function App() {
 
   const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch('https://eo30byi91bvqxol.m.pipedream.net', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          cart,
-          deliveryAddress: DeliveryAddress,
-          userInfo,
-        }),
-      });
-  
-      if (response.ok) {
-        alert('Order submitted successfully!');
-      } else {
-        // Handle error response
-        alert('Failed to submit order. Please try again later.');
-      }
-    } catch (error) {
-      console.error('Error submitting order:', error);
-      alert('An error occurred while submitting the order.');
-    }
-  };
-
-  const handleUserInfoChange = (newUserInfo) => {
-    setUserInfo(prevUserInfo => ({
-      ...prevUserInfo,
-      ...newUserInfo
-    }));
-  };
-  
   return (
     <div className="App">
       <header>
@@ -89,16 +36,7 @@ function App() {
           <TotalAmount cart={cart} />
         </div>
       </main>
-      {totalItems > 0 && <DeliveryAddress onUserInfoChange={handleUserInfoChange} />}
-    <input
-      type="checkbox"
-      checked={termsAccepted}
-      onChange={(e) => setTermsAccepted(e.target.checked)}
-    />
-    <label htmlFor="termsAccepted">I accept the terms & conditions</label>
-    <button onClick={handleSubmit} disabled={!termsAccepted}>
-      Submit Order
-    </button>
+      {totalItems > 0 && <DeliveryAddress />}
     </div>
   );
 }
