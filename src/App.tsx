c658ebcf2857f982d00f75de49a912b2fbd79f95
yusufkara <s215824@dtu.dk>
2024-03-16 11:23:50 +0100

    Navigation and checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,77 @@
+// App.js
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import './assets/styles/App.css';
 import CartList from './components/cart_list/CartList';
 import TotalAmount from './components/total_amount/TotalAmount';
 import { CartItemType } from './lib/types';
 import Cart from './data/Cart';
-import { DeliveryAddress } from './components/delivery_address/DeliveryAddress';
+import Checkout from './components/checkout/Checkout';
 
-function App() {
+// Home page
+const Home = () => {
+  const navigate = useNavigate();
   const [cart, setCart] = useState<CartItemType[]>([]);
 
   useEffect(() => {
-    const initialCart = Cart.getInitialCart().map(item => {
-
-      return {
-        ...item
-      };
-    });
-
+    const initialCart = Cart.getInitialCart();
     setCart(initialCart);
   }, []);
 
   const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+  
+  const handleNavigateToCheckout = () => {
+    navigate('/checkout');
+  };
 
   return (
-    <div className="App">
+    <>
+      <div className="cart-container">
+        <CartList cart={cart} setCart={setCart} />
+      </div>
+      <div className="order-summary">
+        <TotalAmount cart={cart} />
+        {totalItems > 0 && (
+          <button onClick={handleNavigateToCheckout} className="proceed-checkout-btn">
+            Checkout
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+function AppHeader() {
+  const location = useLocation(); // This is fine as AppHeader is rendered inside Router
+
+  if (location.pathname === '/checkout') {
+    return (
       <header>
-        <h1>Cart</h1>
-        <p>You have ({totalItems}) items in total</p>
+        <h1>Checkout</h1>
       </header>
-      <main className="checkout-layout">
-        <div className="cart-container">
-          <CartList cart={cart} setCart={setCart} />
-        </div>
-        <div className="order-summary">
-          <TotalAmount cart={cart} />
-        </div>
-      </main>
-      {totalItems > 0 && <DeliveryAddress />}
-    </div>
+    );
+  }
+
+  return (
+    <header>
+      <h1>Your Cart</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <AppHeader />
+        <main className="checkout-layout">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/checkout/Checkout.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { DeliveryAddress } from '../delivery_address/DeliveryAddress';
+
+import { useNavigate } from 'react-router-dom';
+
+const Checkout = () => {
+  // const history = useHistory(); // For React Router v5
+  const navigate = useNavigate(); // v6
+  const [orderSubmitted, setOrderSubmitted] = useState(false);
+
+  const handleSubmitOrder = async () => {
+    // Implement the logic to submit the order
+    
+    // For demonstration, let's assume the order is submitted successfully
+    console.log('Order is submitted');
+    
+    // Set order submitted to true
+    setOrderSubmitted(true);
+
+
+    navigate('/order-success'); // Navigate to the order success page
+  };
+
+  return (
+    <div className="checkout-container">
+      {!orderSubmitted ? (
+        <>
+          <DeliveryAddress />
+          <button onClick={handleSubmitOrder} className="submit-order-btn">
+            Submit Order
+          </button>
+        </>
+      ) : (
+        <div className="order-success-message">
+          <h2>Thank you!</h2>
+          <p>Your order has been submitted successfully.</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Checkout;
